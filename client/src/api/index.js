837a9b3b9311955c5a0e8639e2239066b8923c28
RishavChanda19 <rishavchanda@flipr.ai>
2023-11-08 01:51:30 +0530

    updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://heliverse-e9xm.onrender.com/api/",
 });
 
 export const getUsers = async ({
